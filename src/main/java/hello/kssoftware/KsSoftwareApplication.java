5df7b73f8b88077b7c756f29f607f9cb5ecfd295
Minuring <mw9505@gmail.com>
2024-07-11 19:54:22 +0900

    프로젝트 연동

---
+++ src/main/java/hello/kssoftware/KsSoftwareApplication.java
@@ -0,0 +1,13 @@
+package hello.kssoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsSoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KsSoftwareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/kssoftware/KsSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.kssoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsSoftwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
