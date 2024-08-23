b54c3492adeec4b519df2bbde9591b77a7e5db44
Minuring <mw9505@gmail.com>
2024-08-23 18:13:43 +0900

    feature/#25-게시판 댓글-작성/수정/삭제 구현

--- src/main/java/hello/kssoftware/board/BoardService.java
+++ src/main/java/hello/kssoftware/board/BoardService.java
@@ -1,8 +1,8 @@
 package hello.kssoftware.board;
 
-import hello.kssoftware.board.dto.BoardCreateDto;
-import hello.kssoftware.board.dto.BoardSearchDto;
-import hello.kssoftware.board.dto.BoardUpdateDto;
+import hello.kssoftware.board.dto.*;
+import hello.kssoftware.board.repository.BoardRepository;
+import hello.kssoftware.board.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +12,15 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 @Transactional
 public class BoardService {
-    @Autowired
-    private BoardRepository boardRepository;
+    private final BoardRepository boardRepository;
+    private final CommentRepository commentRepository;
 
     public Long createBoard(BoardCreateDto createDto) {
         Board board = new Board();
@@ -61,5 +62,27 @@ public class BoardService {
 
     private LocalDateTime now() {
         return LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+    }
+
+    public Comment createComment(Long boardId, CommentCreateDto createDto) {
+        Board board = boardRepository.findById(boardId);
+
+        //임시 (로그인 서비스로부터 작성자 추출해야함)
+        if (createDto.getWriter() == null) {
+            createDto.setWriter("testWriter");
+        }
+        Comment comment = Comment.createComment(board, createDto, now());
+
+        return commentRepository.save(comment);
+    }
+
+    public void updateComment(Long boardId, CommentUpdateDto updateDto) {
+        Comment comment = commentRepository.findById(updateDto.getCommentId()).get();
+
+        comment.update(updateDto.getContent(), now());
+    }
+
+    public void deleteComment(Long boardId, Long commentId) {
+        commentRepository.deleteById(commentId);
     }
 }
---
+++ src/main/java/hello/kssoftware/board/Comment.java
@@ -0,0 +1,53 @@
+package hello.kssoftware.board;
+
+import hello.kssoftware.board.dto.CommentCreateDto;
+import hello.kssoftware.board.dto.CommentUpdateDto;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+    private String writer;
+    private boolean anonymousYn;
+    private String content;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+
+    public static Comment createComment(Board board,CommentCreateDto dto, LocalDateTime createDate) {
+         return new Comment(board,
+                dto.getWriter(),
+                dto.isAnonymousYn(),
+                dto.getContent(),
+                createDate);
+    }
+
+    public void update(String content, LocalDateTime updateDate) {
+        this.content = content;
+        this.updateDate = updateDate;
+    }
+
+
+    protected Comment(Board board, String writer, boolean anonymousYn, String content, LocalDateTime createDate) {
+        this.board = board;
+        this.writer = writer;
+        this.anonymousYn = anonymousYn;
+        this.content = content;
+        this.createDate = createDate;
+    }
+}
