99fc7297ae5140250506c939f3e505ee3788ce92
kim-do-kyun <zetgi7dk5359@gmail.com>
2024-09-23 18:24:00 +0900

    로그인 검증 로그인시 회원 아이디와 비밀번호 매칭 여부 확인 및 불일치 시 에러메시지 송출

---
+++ src/main/java/hello/kssoftware/login/validation/match/Match.java
@@ -0,0 +1,18 @@
+package hello.kssoftware.login.validation.match;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = MatchValidator.class)
+public @interface Match {
+    String message() default "아이디 또는 비밀번호가 잘못 되었습니다. 아이디와 비밀번호를 정확히 입력해 주세요.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
