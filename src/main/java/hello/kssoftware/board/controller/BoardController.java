b576a43d309083a406c1358da623912cd63a2acd
Minuring <mw9505@gmail.com>
2024-09-29 19:28:57 +0900

    게시판, 댓글 수정 및 삭제에서 작성자검증을 인터셉터로 분리
    반복되던 로직인 작성자와 현재 로그인한 유저가 일치하는지 검증을 인터셉터로 분리
    컨트롤러에서는 로그인이 되어있고, 수정과 삭제의 경우 작성자 검증까지 마친 정상 플로우를 단정해도 됨.

--- src/main/java/hello/kssoftware/board/controller/BoardController.java
+++ src/main/java/hello/kssoftware/board/controller/BoardController.java
@@ -12,12 +12,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+/**
+ * {@link hello.kssoftware.login.interceptor.LoginCheckInterceptor}에 의해 로그인이 되어있고,
+ * 수정, 삭제 요청의 경우 {@link hello.kssoftware.board.interceptor.AuthorMatchCheckInterceptor}에 의해
+ * 작성자와 로그인유저 검증을 마친 정상 흐름을 가정하고 처리
+ */
 @Controller
 @RequestMapping("/board")
 @RequiredArgsConstructor
@@ -28,45 +32,46 @@ public class BoardController {
     private final FlashNotifier flashNotifier;
 
     @GetMapping
-    public String getBoardList(@ModelAttribute BoardSearchDto boardSearchDto, Model model) {
-        List<Board> boards = boardService.findAll(boardSearchDto);
+    public String getBoardList(@ModelAttribute("boardSearch") BoardDto.Search search, Model model) {
+        List<BoardDto.Response> boards = boardService.findAll(search)
+                .stream()
+                .map(BoardDto.Response::new)
+                .toList();
+
         model.addAttribute("boards", boards);
         return "board/boards";
     }
 
     @GetMapping("/{id}")
-    public String getBoardDetails(@PathVariable(value = "id") long id, Model model,
-                                  @ModelAttribute CommentCreateDto commentCreateDto,
-                                  @ModelAttribute CommentUpdateDto commentUpdateDto) {
+    public String getBoardDetails(@PathVariable(value = "id") long id, Model model) {
 
-        Board board = boardService.findById(id);
-        List<Comment> comments = board.getComments();
+        Board originBoard = boardService.findById(id);
+        BoardDto.Response board = new BoardDto.Response(originBoard);
+
+        List<Comment> originComments = board.getComments();
+        List<CommentDto.Response> comments = originComments.stream().map(CommentDto.Response::new).toList();
 
         model.addAttribute("board", board);
         model.addAttribute("comments", comments);
+        model.addAttribute("commentCreateDto", new CommentDto.Create());
+        model.addAttribute("commentUpdateDto", new CommentDto.Update());
 
         return "board/board";
     }
 
     @GetMapping("/post")
-    public String getBoardCreateForm(@Login Member member,
-                                     @ModelAttribute BoardCreateDto boardCreateDto) {
+    public String getBoardCreateForm(@ModelAttribute("boardCreateDto") BoardDto.Create dto) {
 
         return "board/post";
     }
 
     @PostMapping("/post")
     public String createBoard(@Login Member member,
-                              @Valid BoardCreateDto createDto,
-                              BindingResult result,
+                              @Valid BoardDto.Create dto,
                               RedirectAttributes redirectAttributes) {
 
-        if (result.hasErrors()) {
-            return "board/post";
-        }
-
-        createDto.setWriter(member);
-        Long id = boardService.createBoard(createDto);
+        dto.setWriter(member);
+        Long id = boardService.createBoard(dto);
         flashNotifier.notify("message.created.board");
 
         redirectAttributes.addAttribute("id", id);
@@ -74,54 +79,31 @@ public class BoardController {
     }
 
     @GetMapping("/{id}/edit")
-    public String getBoardUpdateForm(@Login Member member,
-                                     @PathVariable(value = "id") Long id, Model model) {
+    public String getBoardUpdateForm(@PathVariable(value = "id") Long id, Model model) {
 
-        Board board = boardService.findById(id);
+        Board originBoard = boardService.findById(id);
+        BoardDto.Response board = new BoardDto.Response(originBoard);
 
-        if (member.equals(board.getWriter())) {
-            model.addAttribute("board", board);
-            return "board/edit";
-
-        } else {
-            flashNotifier.notify("not_author");
-            return "redirect:/board/{id}";
-
-        }
+        model.addAttribute("board", board);
+        return "board/edit";
     }
 
     @PostMapping("/{id}/edit")
-    public String updateBoard(@Login Member member,
-                              @PathVariable(value = "id") Long id, BoardUpdateDto updateDto) {
+    public String updateBoard(BoardDto.Update dto) {
 
-        Board board = boardService.findById(id);
-        if (member.equals(board.getWriter())) {
-            boardService.updateBoard(id, updateDto);
-            flashNotifier.notify("message.updated.board");
+        boardService.updateBoard(dto);
+        flashNotifier.notify("message.updated.board");
 
-        } else {
-            flashNotifier.notify("not_author");
-
-        }
-        
         return "redirect:/board/{id}";
     }
 
     @PostMapping("/{id}/delete")
-    public String deleteBoard(@Login Member member,
-                              @PathVariable(value = "id") Long id) {
+    public String deleteBoard(@PathVariable(value = "id") Long id) {
 
-        Board board = boardService.findById(id);
-        if (member.equals(board.getWriter())) {
-            boardService.deleteBoard(id);
-            flashNotifier.notify("message.deleted.board");
-            return "redirect:/board";
+        boardService.deleteBoard(id);
 
-        } else {
-            flashNotifier.notify("not_author");
-            return "redirect:/board/{id}";
-
-        }
-
+        flashNotifier.notify("message.deleted.board");
+        return "redirect:/board";
     }
+
 }
