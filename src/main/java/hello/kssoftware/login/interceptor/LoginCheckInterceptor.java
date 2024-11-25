e2a86417d34c9b827801de1a7d8989419a635fe4
Minuring <mw9505@gmail.com>
2024-11-25 18:46:20 +0900

    refactor: GET요청일때만 되돌아가도록 변경
    GET요청이 아닐 때 로그인 후 해당 위치로 돌아가지 않기 위해 변경

--- src/main/java/hello/kssoftware/login/interceptor/LoginCheckInterceptor.java
+++ src/main/java/hello/kssoftware/login/interceptor/LoginCheckInterceptor.java
@@ -7,24 +7,34 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.io.IOException;
+
 @RequiredArgsConstructor
 public class LoginCheckInterceptor implements HandlerInterceptor {
 
     private final FlashNotifier flashNotifier;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        String requestURI = request.getRequestURI();
-
-        HttpSession session = request.getSession();
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        HttpSession session = request.getSession(false);
 
         if (session == null || session.getAttribute("loginUser") == null) {
             flashNotifier.notify("required_login");
-            response.sendRedirect("/login/signIn?redirectURI=" + requestURI);
+            sendRedirect(request, response);
             return false;
         }
 
         return true;
+    }
+
+    private void sendRedirect(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String httpMethod = request.getMethod();
+
+        if ("GET".equals(httpMethod)) {
+            String requestURI = request.getRequestURI();
+            response.sendRedirect("/login/signIn?redirectURI=" + requestURI);
+            return;
+        }
+        response.sendRedirect("/login/signIn");
     }
 }
